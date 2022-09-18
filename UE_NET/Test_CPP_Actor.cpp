c9596dcc1e26ce3d04913a4ad4f201688255f818
Chongzi-K <xie-zi-wei@foxmail.com>
2022-09-18 22:53:03 +0800

    no message

--- UE_NET/Test_CPP_Actor.cpp
+++ UE_NET/Test_CPP_Actor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Test_CPP_Actor.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 ATest_CPP_Actor::ATest_CPP_Actor()
@@ -9,12 +10,22 @@ ATest_CPP_Actor::ATest_CPP_Actor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//启用复制功能
+	//服务端调用spawn时，客户端也会生成，并且是由服务端复制信息到客户端，属于“同一个”actor
+	//不勾选的时候，每个端自己决定是否生成，生成出来的也不是同一个actor
+	bReplicates = true;
+
 }
 
 // Called when the game starts or when spawned
 void ATest_CPP_Actor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		Health = 500.0f;
+	}
 	
 }
 
@@ -23,5 +34,35 @@ void ATest_CPP_Actor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Sever Health= %f"),Health);
+
+		Armor = FMath::Rand();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Client Health= %f"), Health);
+	}
+
 }
 
+void ATest_CPP_Actor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps)
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ATest_CPP_Actor, Health);
+	DOREPLIFETIME(ATest_CPP_Actor,Armor);
+}
+
+void ATest_CPP_Actor::OnRep_Armor()
+{
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Sever Armor= %f"), Armor);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Client Armor= %f"), Armor);
+	}
+}
--- UE_NET/Test_CPP_Actor.h
+++ UE_NET/Test_CPP_Actor.h
@@ -23,4 +23,31 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere,Replicated)//服务端生成的actor的数据会覆盖客户端生成
+	float Health = 100.0f;
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Armor)
+	float Armor;
+
+	UFUNCTION()
+	void OnRep_Armor();
+
 };
+
+
+
+
+
+/*
+Res_Notify 复制通知
+如果一个变量设置为Res_Notify，当他发生复制时，服务端和收到该通知的客户端都能调用一个自定义函数
+C++中仅在客户端调用函数，在服务端中调用需要采用RPC
+*/
+
+/*
+RPC 可以实现 服务端调用客户端函数，客户端调用服务端函数
+不可以有返回值
+默认是不可靠的，需要 reliable 使其可靠
+C++ 中 要把 Server \ Client \ NetMulticast 关键字添加到UFUNCTION中
+蓝图中因为不能有返回值，所以不能用函数做RPC，要用事件
+*/
